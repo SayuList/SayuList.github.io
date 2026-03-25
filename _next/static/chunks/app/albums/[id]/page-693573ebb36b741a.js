b13686c319be42564fa2acde185608920d68bc45
SayuList <sayulistadmin@gmail.com>
2026-03-26 00:14:00 +0900

    Fix CSS 404: Add .nojekyll and update next.config.js

---
+++ _next/static/chunks/app/albums/[id]/page-693573ebb36b741a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[271,931,187,366,420,389,154,282,906],{7886:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=7886)}),_N_E=n.O()}]);
