8817d4012f8a2c7853488f9c65b541427de0e931
SayuList <sayulistadmin@gmail.com>
2026-05-10 03:18:05 +0900

    Update version to 0.1.1 (fixed source)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
           当サイトはファン有志が運営しており、<br className="mobile-br" />
           情報の正確性・網羅性は保証しておりません。
         </p>
-        <p className="text-right" style={{ marginTop: 'var(--spacing-unit)' }}>ver0.1.0 / SayuList</p>
+        <p className="text-right" style={{ marginTop: 'var(--spacing-unit)' }}>ver0.1.1 / SayuList</p>
       </footer>
     );
   }
